d01f5bb11335df713c1aca97f5da0974c8a6c71a
UvinduHewage <uvinduudakaramagedarah@gmail.com>
2025-05-01 23:24:40 +0530

    some deleted files in backend

--- cooking_backend/src/main/java/com/PAF/CookingPostAdding/services/Bawantha_service/RecipeService.java
+++ cooking_backend/src/main/java/com/PAF/CookingPostAdding/services/Bawantha_service/RecipeService.java
@@ -14,7 +14,7 @@ public class RecipeService {
 
     @Autowired
     private RecipeRepository recipeRepository;
-    
+
     public Recipe createRecipe(Recipe recipe, String author) {
         recipe.setAuthor(author);
         recipe.setCreatedAt(LocalDateTime.now());
@@ -25,7 +25,7 @@ public class RecipeService {
 
     public Recipe getRecipe(String id) {
         return recipeRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Recipe not found"));
+                .orElseThrow(() -> new RuntimeException("Recipe not found with ID: " + id));
     }
 
     public List<Recipe> getAllRecipes() {
@@ -37,47 +37,18 @@ public class RecipeService {
     }
 
     public Recipe updateRecipe(String id, Recipe updatedRecipe, String author) {
-        Recipe recipe = getRecipe(id);
-        recipe.setTitle(updatedRecipe.getTitle());
-        recipe.setDescription(updatedRecipe.getDescription());
-        recipe.setMediaUrls(updatedRecipe.getMediaUrls());
-        recipe.setSteps(updatedRecipe.getSteps());
-        recipe.setUpdatedAt(LocalDateTime.now());
-        return recipeRepository.save(recipe);
+        Recipe existingRecipe = getRecipe(id);
+        existingRecipe.setTitle(updatedRecipe.getTitle());
+        existingRecipe.setDescription(updatedRecipe.getDescription());
+        existingRecipe.setMediaUrls(updatedRecipe.getMediaUrls());
+        existingRecipe.setSteps(updatedRecipe.getSteps());
+        existingRecipe.setUpdatedAt(LocalDateTime.now());
+        return recipeRepository.save(existingRecipe);
     }
-    
 
     public void deleteRecipe(String id, String author) {
         Recipe recipe = getRecipe(id);
         recipeRepository.delete(recipe);
     }
-
-    // // Share a post publicly
-    // public Recipe sharePost(String id) {
-    //     Recipe recipe = getRecipe(id);
-    //     recipe.setShared(true);
-    //     return recipeRepository.save(recipe);
-    // }
-
-    // public Recipe addComment(String id, String comment) {
-    //     Recipe recipe = getRecipe(id);
-    //     recipe.getComments().add(comment);
-    //     return recipeRepository.save(recipe);
-    // }
-
-    // public Recipe updateComment(String id, int index, String newComment) {
-    //     Recipe recipe = getRecipe(id);
-    //     if (index >= 0 && index < recipe.getComments().size()) {
-    //         recipe.getComments().set(index, newComment);
-    //     }
-    //     return recipeRepository.save(recipe);
-    // }
-    
-    // public Recipe deleteComment(String id, int index) {
-    //     Recipe recipe = getRecipe(id);
-    //     if (index >= 0 && index < recipe.getComments().size()) {
-    //         recipe.getComments().remove(index);
-    //     }
-    //     return recipeRepository.save(recipe);
-    // }
 }
+
