cb5988a2f25d90d13f50947f602eec3caed878d4
UvinduHewage <uvinduudakaramagedarah@gmail.com>
2025-05-08 12:29:15 +0530

    # commit

--- cooking_backend/src/main/java/com/PAF/CookingPostAdding/auth/service/CustomUserDetailsService.java
+++ cooking_backend/src/main/java/com/PAF/CookingPostAdding/auth/service/CustomUserDetailsService.java
@@ -3,11 +3,11 @@ package com.PAF.CookingPostAdding.auth.service;
 import com.PAF.CookingPostAdding.auth.model.User;
 import com.PAF.CookingPostAdding.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collections;
 
@@ -21,11 +21,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-
+        
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
-        );
+                Collections.singletonList(new SimpleGrantedAuthority("USER")));
     }
 }
